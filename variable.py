dda4fb1fd0b988c0ec3c38fa58cea72d7e02bf18
sandykri09 <115703445+sandykri09@users.noreply.github.com>
2022-10-16 02:40:37 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,81 @@
+# a=1
+# print(a) # 1
+
+# A=2
+# _b=3
+# _1=4
+# c_a=5
+# v_3=6
+# _=7
+# x_y3=8
+# r6=9
+# t_4_d=10
+# print(A) # 2
+# print(_b) # 3
+# print(_1) # 4
+# print(c_a) # 5
+# print(v_3) # 6
+# print(_) # 7
+# print(x_y3) # 8
+# print(r6) # 9
+# print(t_4_d) # 10 
+# print(bs) # NameError: name 'bs' is not defiend
+# print(xyz) # NameError : name 'xyz' is not defiend
+
+# a='python'
+# print(a.isidentifier) # 'str'
+
+# b="hello"
+# print(b.isidentifier) # 'str'
+ 
+# c="23hh"
+# print(c.isidentifier) # "str"
+
+"""ASSIGN VARIABLE"""
+
+"""Termination:- indicates(;) means totally ending more than one variable in one line have to seperate with."""
+# a=1;b=2;c=3
+# print(a);print(b);print(c) #1 next line 2, next line 3
+
+# a=6
+# print(a) # 6
+# print(a); print(a) # 6 next line 6
+
+# a=9
+# print(a) # 9
+# print(a,a) # 9 9
+
+# a=1; b=2; c=3
+# print(a,b,c) # 1 2 3
+
+# a=c=5; b=4
+# print(a,b,c) # 5 4 5
+
+# p=2
+# q=3
+# r=4
+# s=5
+# print(p) # 2
+# print(q) # 3
+# print(r) # 4
+# print(s) # 5
+# print(p,q,r,s) # 2 3 4 5
+
+""""2nd ways to assigning the values and printing them"""
+
+# p=2; q=3; r=4; s=5
+# print(p); print(q); print(r); print(s) 
+
+"""3rd ways to assigning the values and printing them"""
+
+# p=r=4; q=2; s=5
+# print(p); print(q); print(r); print(s)
+
+"""4th ways to assigning the values and printing them"""
+
+p,q,r,s=1,2,3,4
+print(p); print(q); print(r); print(s)
+ 
+a=6
+b=5
+print(a+b) # 11
